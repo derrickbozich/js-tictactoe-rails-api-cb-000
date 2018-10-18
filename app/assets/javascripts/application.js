6e27d1df41995d21e53367606abd61508f1789e2
derrick b <dbozich1@gmail.com>
2018-10-18 17:58:42 +0000

    automatically backed up by learn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require handlebars
 //= require jquery
 //= require jquery_ujs
 //= require tictactoe
