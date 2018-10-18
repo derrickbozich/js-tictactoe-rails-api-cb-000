085c2d3f5130ae187b488b40dd168d4702fffc75
derrick b <dbozich1@gmail.com>
2018-10-18 18:55:50 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -116,9 +116,10 @@ function previousGames() {
 
 function openSpaces(){
   let board = getBoard();
+  let array
   // let result = board.every((space) => space != '' )
   let result = board.every((space) => {
-    space == 'X' || space == 'O'
+    return space == ''
   })
   debugger
 }
